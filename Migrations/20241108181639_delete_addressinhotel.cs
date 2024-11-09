3b904ac184bc0a6e504e0b42c3994ff5363933d2
Vu Tien Thanh <vutienthanh25092003@gmail.com>
2024-11-09 09:43:28 +0700

    git add "add gg map APi but don't have API key"

---
+++ Migrations/20241108181639_delete_addressinhotel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class delete_addressinhotel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Hotels");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
